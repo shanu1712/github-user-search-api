6d14399f24ad6b353a15e7cdb0f6fc27b4d7a817
Shanu Chaturvedi <93817094+shanu1712@users.noreply.github.com>
2022-09-24 00:34:00 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,26 @@
+console.log('Hello!');
+
+const form = document.getElementById('form');
+
+form.addEventListener('submit', function (e) {
+  e.preventDefault();
+
+  const search = document.getElementById('search').value;
+  const originalname = search.split(' ').join(''); //removing the spaces and joining the string
+  //alert(originalname);
+  document.getElementById('result').innerHTML = '';
+  console.log(search); /////checking that the value is getting printed or not
+  //fetch("https://api.github.com/users"+search);
+  fetch('https://api.github.com/users/' + originalname)
+    .then((result) => result.json())
+    .then((data) => {
+      console.log(data);
+
+      document.getElementById(
+        'result'
+      ).innerHTML = `<a target="_blank" href="https://www.github.com/${originalname}"><img src="${data.avatar_url}"/></a>
+     <l1>${data.name}</l1>
+     <li>${data.bio}</li>`;
+      /// targetting with anchor tag and displai=ying the search result for the users in the next window
+    });
+});
